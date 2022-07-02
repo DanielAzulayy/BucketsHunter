376c42851c79e1c0b29b95e17d13706278188643
DanielAzulay9 <azulay459@gmail.com>
2022-07-02 20:19:39 +0300

    changed configs names

--- BucketsHunter/conf/__init__.py
+++ BucketsHunter/conf/__init__.py
@@ -1,17 +1,19 @@
 from dataclasses import dataclass
 from typing import List, Optional
-from dns.resolver import Resolver
+
+from utils.dns import DNSUtils
 
 
 @dataclass
 class Config:
     """Config dataclass in order to create relevant data for all scans
-    - dns_resolver: dns resolver for dns lookups
+    - dns_utils: dns utilities for dns lookups
     - output_file: output file - saving scan results
     - buckets_permutations: lists with different buckets to bruteforce
+    - directory_wordlist: bucket directory bruteforce
     """
 
-    dns_resolver: Resolver = None
+    dns_utils: DNSUtils = None
     output_file: Optional[str] = None
     buckets_permutations: List[str] = None
     directory_wordlist: List[str] = None
